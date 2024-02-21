cfd634a0fc7be3ac5b10e82dccca0a9b94c63ade
m4rcotoni <marcosanto.sl021@academico.ifsul.edu.br>
2024-02-21 19:51:52 -0300

    test 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Stack, Link } from 'expo-router';
 import { useEffect, useRef, useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Alert, Dimensions, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import {
   MediaStream,
   RTCPeerConnection,
@@ -10,29 +10,19 @@ import {
   RTCView,
 } from 'react-native-webrtc';
 
-// const peerConstraints = {
-//   iceServers: [
-//     {
-//       urls: 'stun:stun.l.google.com:19302',
-//     },
-//   ],
-// };
-// const peerConnection = new RTCPeerConnection(peerConstraints);
-// const datachannel = peerConnection.createDataChannel('datachannel');
-// peerConnection.addEventListener('datachannel', (event) => {
-//   const datachannel = event.channel;
-
-//   console.log('Got datachannel: ', datachannel);
-
-//   // Now you've got the datachannel.
-//   // You can hookup and use the same events as above ^
-// });
-// datachannel.addEventListener('message', (message) => {
-//   console.log('Got message: ', message);
-// });
+const peerConstraints = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.1.google.com:19302', 'stun:stun2.1.google.com:19302'],
+    },
+  ],
+};
+const peerConnection = new RTCPeerConnection(peerConstraints);
 export default function Page() {
   const [localMediaStream, setLocalMediaStream] = useState<MediaStream | null>(null);
   const [remoteMediaStream, setRemoteMediaStream] = useState<MediaStream | null>(null);
+  const [offerDescriptionText, setOfferDescriptionText] = useState<string>('');
+  const [answerDescription, setAnswerDescription] = useState<string>('');
   const [gettingCall, setGettingCall] = useState(false);
   const pc = useRef<RTCPeerConnection | null>(null);
 
@@ -51,103 +41,33 @@ export default function Page() {
     },
   };
 
-  const peerConstraints = {
-    iceServers: [
-      {
-        urls: 'stun:stun.l.google.com:19302',
-      },
-    ],
-  };
-
-  const peerConnection = new RTCPeerConnection(peerConstraints);
-
-  peerConnection.addEventListener('connectionstatechange', (event) => {
-    switch (peerConnection.connectionState) {
-      case 'closed':
-        // You can handle the call being disconnected here.
-
-        break;
-    }
-  });
-
-  peerConnection.addEventListener('icecandidate', (event) => {
-    // When you find a null candidate then there are no more candidates.
-    // Gathering of candidates has finished.
-    if (!event.candidate) {
-    }
-
-    // Send the event.candidate onto the person you're calling.
-    // Keeping to Trickle ICE Standards, you should send the candidates immediately.
-  });
-
-  peerConnection.addEventListener('icecandidateerror', (event) => {
-    // You can ignore some candidate errors.
-    // Connections can still be made even when errors occur.
-  });
-
-  peerConnection.addEventListener('iceconnectionstatechange', (event) => {
-    switch (peerConnection.iceConnectionState) {
-      case 'connected':
-      case 'completed':
-        // You can handle the call being connected here.
-        // Like setting the video streams to visible.
-
-        break;
-    }
-  });
-
-  peerConnection.addEventListener('negotiationneeded', (event) => {
-    // You can start the offer stages here.
-    // Be careful as this event can be called multiple times.
-  });
-
-  peerConnection.addEventListener('signalingstatechange', (event) => {
-    switch (peerConnection.signalingState) {
-      case 'closed':
-        // You can handle the call being disconnected here.
-
-        break;
-    }
-  });
-
-  peerConnection.addEventListener('track', (event) => {
-    // Grab the remote track from the connected participant.
-    setRemoteMediaStream(remoteMediaStream || new MediaStream());
-    if (remoteMediaStream) {
-      remoteMediaStream.addTrack(event.track, remoteMediaStream);
-    }
-  });
-
-  let remoteCandidates = [];
-
-  function handleRemoteCandidate(iceCandidate) {
-    iceCandidate = new RTCIceCandidate(iceCandidate);
-
-    if (peerConnection.remoteDescription == null) {
-      return remoteCandidates.push(iceCandidate);
-    }
-
-    return peerConnection.addIceCandidate(iceCandidate);
-  }
-
-  function processCandidates() {
-    if (remoteCandidates.length < 1) {
-      return;
-    }
-
-    remoteCandidates.map((candidate) => peerConnection.addIceCandidate(candidate));
-    remoteCandidates = [];
-  }
-
   const createOffer = async () => {
     try {
+      const remoteMediaStream = new MediaStream();
+      setRemoteMediaStream(remoteMediaStream);
+      if (!localMediaStream) return;
+      localMediaStream
+        .getTracks()
+        .forEach((track) => peerConnection.addTrack(track, localMediaStream));
+
+      peerConnection.addEventListener('track', (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteMediaStream.addTrack(track);
+        });
+      });
+
+      peerConnection.addEventListener('icecandidate', (event) => {
+        if (event.candidate) {
+          console.log('event.candidate: ', event.candidate);
+        }
+      });
+
       const offerDescription = await peerConnection.createOffer(sessionConstraints);
       await peerConnection.setLocalDescription(offerDescription);
-
-      console.log('offerDescription: ', offerDescription);
-      // Send the offerDescription to the other participant.
-
-      processCandidates();
+      setOfferDescriptionText(JSON.stringify(offerDescription));
+      console.log('offerDescription: ', JSON.stringify(offerDescription));
+      // Alert.alert('offerDescription', JSON.stringify(offerDescription));
+      // processCandidates();
     } catch (err) {
       console.log('Error: ', err);
     }
@@ -155,124 +75,56 @@ export default function Page() {
 
   const createAnswer = async () => {
     try {
-      const answerDescription = await peerConnection.createAnswer();
-      await peerConnection.setLocalDescription(answerDescription);
-
-      // Send the answerDescription to the other participant.
-      console.log('answerDescription: ', answerDescription);
-    } catch (err) {
-      // Handle Errors
-    }
-  };
-
-  const setup = async () => {
-    try {
-      // Use the received offerDescription
-      const offerDescription = new RTCSessionDescription(offerDescription);
-      await peerConnection.setRemoteDescription(offerDescription);
-
-      const answerDescription = await peerConnection.createAnswer();
-      await peerConnection.setLocalDescription(answerDescription);
-
-      // Here is a good place to process candidates.
-      processCandidates();
-
-      // Send the answerDescription back as a response to the offerDescription.
-    } catch (err) {
-      // Handle Errors
-    }
-  };
-  const countMediaDevices = async () => {
-    let cameraCount = 0;
-
-    try {
-      const devices = await mediaDevices.enumerateDevices();
-
-      console.log('devices: ', devices[0]);
-
-      devices.map((device) => {
-        if (device.kind !== 'videoinput') {
-          return;
-        }
-
-        cameraCount = cameraCount + 1;
-      });
-    } catch (err) {
-      console.log('Error: ', err);
-    }
-    console.log('cameraCount: ', cameraCount);
-
-    return cameraCount;
-  };
-
-  const getMedia = async () => {
-    const isVoiceOnly = false;
-
-    try {
-      const mediaStream = await mediaDevices.getUserMedia(mediaConstraints);
-
-      if (isVoiceOnly) {
-        const videoTrack = await mediaStream.getVideoTracks()[0];
-        videoTrack.enabled = false;
-      }
-
-      setLocalMediaStream(mediaStream);
-      console.log('localMediaStream: ', mediaStream);
-      if (mediaStream) {
-        addMediaStream();
-        getDisplayMedia();
-      }
-    } catch (err) {
-      // Handle Error
-      console.log('Error: ', err);
-    }
-  };
-  // const addMediaStream = () => {
-  //   if (localMediaStream) {
-  //     localMediaStream
-  //       .getTracks()
-  //       .forEach((track) => peerConnection.addTrack(track, localMediaStream));
-  //   }
-  // }
-
-  const getDisplayMedia = async () => {
-    try {
-      const mediaStream = await mediaDevices.getDisplayMedia();
-
-      console.log('mediaStream: ', mediaStream);
-    } catch (err) {
-      console.log('Error: ', err);
-    }
-  };
-
-  const stopLocalStream = () => {
-    if (localMediaStream) {
-      localMediaStream.getTracks().forEach((track) => {
-        track.stop();
-      });
-      setLocalMediaStream(null);
-
-      // You can also remove the tracks from the peerConnection.
-      peerConnection.getSenders().forEach((sender) => {
-        peerConnection.removeTrack(sender);
-      });
-
-      // You can also close the peerConnection.
-      peerConnection.close();
-    }
-  };
-
-  const addMediaStream = () => {
-    if (localMediaStream) {
+      const remoteMediaStream = new MediaStream();
+      setRemoteMediaStream(remoteMediaStream);
+      if (!localMediaStream) return;
       localMediaStream
         .getTracks()
         .forEach((track) => peerConnection.addTrack(track, localMediaStream));
+
+      peerConnection.addEventListener('track', (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteMediaStream.addTrack(track);
+        });
+      });
+
+      peerConnection.addEventListener('icecandidate', (event) => {
+        if (event.candidate) {
+          console.log('event.candidate: ', event.candidate);
+          setOfferDescriptionText(JSON.stringify(peerConnection.localDescription));
+        }
+      });
+
+      const answerDescription = await peerConnection.createAnswer();
+      await peerConnection.setRemoteDescription(answerDescription);
+      setAnswerDescription(JSON.stringify(answerDescription));
+
+      console.log('answerDescription: ', answerDescription);
+      // Alert.alert('answerDescription', JSON.stringify(answerDescription));
+      // processCandidates();
+    } catch (err) {
+      console.log('Error: ', err);
     }
   };
-  // const sendMessage = () => {
-  //   datachannel.send('Hey There!');
-  // };
+  const addAnswer = async () => {
+    try {
+      if (!peerConnection.remoteDescription) {
+        peerConnection.setRemoteDescription(answerDescription);
+      }
+    } catch (err) {
+      // Handle Errors
+    }
+  };
   console.log('localMediaStream ', localMediaStream?.toURL() || 'No Stream');
+  const { height, width } = Dimensions.get('window');
+
+  useEffect(() => {
+    async function getMedia() {
+      const mediaStream = await mediaDevices.getUserMedia(mediaConstraints);
+      setLocalMediaStream(mediaStream);
+    }
+    getMedia();
+  }, []);
 
   return (
     <View className={styles.container}>
@@ -286,17 +138,49 @@ export default function Page() {
         <RTCView
           mirror
           objectFit="cover"
+          streamURL={remoteMediaStream ? remoteMediaStream.toURL() : ''}
+          zOrder={0}
+          style={{
+            width: 90,
+            height: 140,
+            position: 'absolute',
+            bottom: 90,
+            right: 15,
+            zIndex: 1,
+          }}
+        />
+        <RTCView
+          mirror
+          objectFit="cover"
           streamURL={localMediaStream ? localMediaStream.toURL() : ''}
           zOrder={0}
-          style={{ width: 300, height: 300 }}
+          style={{ width, height: height - 60 }}
         />
+
         <TouchableOpacity className={`${styles.button} bg-green-500`} onPress={createOffer}>
-          <Text className={styles.buttonText}>createOffer</Text>
+          <Text className={styles.buttonText}>Offer</Text>
         </TouchableOpacity>
-        <TouchableOpacity className={`${styles.button} bg-blue-500`} onPress={createAnswer}>
-          <Text className={styles.buttonText}>createAnswer</Text>
+        <TouchableOpacity
+          className={`${styles.button} bg-blue-500 bottom-20`}
+          onPress={createAnswer}>
+          <Text className={styles.buttonText}>Answer</Text>
         </TouchableOpacity>
-        <TouchableOpacity className={styles.button} onPress={getMedia}>
+
+        <TouchableOpacity className={`${styles.button} bg-red-500 bottom-40`} onPress={addAnswer}>
+          <Text className={styles.buttonText}>Add Answer</Text>
+        </TouchableOpacity>
+
+        <TextInput
+          placeholder="Offer Description"
+          value={offerDescriptionText}
+          className="absolute top-0 left-0 w-[200px] h-14 bg-white z-10"
+        />
+        <TextInput
+          placeholder="Answer Description"
+          value={answerDescription}
+          className="absolute top-20 left-0 w-[200px] h-14 bg-white z-10"
+        />
+        {/*<TouchableOpacity className={styles.button} onPress={getMedia}>
           <Text className={styles.buttonText}>Get Local Media</Text>
         </TouchableOpacity>
         <TouchableOpacity className={styles.button} onPress={addMediaStream}>
@@ -307,16 +191,16 @@ export default function Page() {
         </TouchableOpacity>
         <TouchableOpacity className={`${styles.button} bg-gray-600`} onPress={setup}>
           <Text className={styles.buttonText}>Setup</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   );
 }
 
 const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4 w-[200] m-4',
+  button: 'items-center rounded-[28px] shadow-md p-4 w-[200] m-4 absolute bottom-0 left-0',
   buttonText: 'text-white text-lg font-semibold text-center',
-  container: 'flex-1 p-6',
+  container: 'flex-1',
   main: 'flex-1 max-w-[960] mx-auto justify-center items-center ',
   title: 'text-[64px] font-bold',
   subtitle: 'text-4xl text-gray-700',
