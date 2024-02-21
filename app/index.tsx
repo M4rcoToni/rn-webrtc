9848019e9a24a6a527ddf2bfcf99e70071c51fdb
m4rcotoni <marcosanto.sl021@academico.ifsul.edu.br>
2024-02-21 20:46:19 -0300

    test 3

--- app/index.tsx
+++ app/index.tsx
@@ -97,24 +97,34 @@ export default function Page() {
 
       const answerDescription = await peerConnection.createAnswer();
       await peerConnection.setRemoteDescription(answerDescription);
-      setAnswerDescription(JSON.stringify(answerDescription));
-
+      setAnswerDescription(answerDescription);
+      setOfferDescriptionText(JSON.stringify(answerDescription));
       console.log('answerDescription: ', answerDescription);
       // Alert.alert('answerDescription', JSON.stringify(answerDescription));
       // processCandidates();
     } catch (err) {
-      console.log('Error: ', err);
+      console.log('Error createAnswer: ', err);
     }
   };
   const addAnswer = async () => {
     try {
-      if (!peerConnection.remoteDescription) {
-        peerConnection.setRemoteDescription(answerDescription);
-      }
+      const answeradd = JSON.parse(answerDescription);
+      peerConnection.setRemoteDescription(answeradd);
+
+      console.log('answeradd: ', answeradd);
+
+      console.log(remoteMediaStream);
     } catch (err) {
       // Handle Errors
     }
   };
+
+  const closeConnection = () => {
+    peerConnection.close();
+    setLocalMediaStream(null);
+    setRemoteMediaStream(null);
+  };
+
   console.log('localMediaStream ', localMediaStream?.toURL() || 'No Stream');
   const { height, width } = Dimensions.get('window');
 
@@ -125,6 +135,7 @@ export default function Page() {
     }
     getMedia();
   }, []);
+  console.log('remoteMediaStream', remoteMediaStream?.toURL());
 
   return (
     <View className={styles.container}>
@@ -138,15 +149,18 @@ export default function Page() {
         <RTCView
           mirror
           objectFit="cover"
-          streamURL={remoteMediaStream ? remoteMediaStream.toURL() : ''}
+          streamURL={remoteMediaStream?.toURL()}
           zOrder={0}
           style={{
             width: 90,
             height: 140,
-            position: 'absolute',
-            bottom: 90,
-            right: 15,
-            zIndex: 1,
+            alignSelf: 'flex-end',
+            // left: 20,
+            // top: 0,
+            // position: 'absolute',
+            // bottom: 90,
+            // right: 15,
+            // zIndex: 1,
           }}
         />
         <RTCView
@@ -154,7 +168,7 @@ export default function Page() {
           objectFit="cover"
           streamURL={localMediaStream ? localMediaStream.toURL() : ''}
           zOrder={0}
-          style={{ width, height: height - 60 }}
+          style={{ width, height: height - 460 }}
         />
 
         <TouchableOpacity className={`${styles.button} bg-green-500`} onPress={createOffer}>
@@ -170,6 +184,12 @@ export default function Page() {
           <Text className={styles.buttonText}>Add Answer</Text>
         </TouchableOpacity>
 
+        <TouchableOpacity
+          className={`${styles.button} bg-red-500 left-56`}
+          onPress={closeConnection}>
+          <Text className={styles.buttonText}>Close</Text>
+        </TouchableOpacity>
+
         <TextInput
           placeholder="Offer Description"
           value={offerDescriptionText}
@@ -182,18 +202,6 @@ export default function Page() {
           onChangeText={setAnswerDescription}
           className="absolute top-20 left-0 w-[200px] h-14 bg-white z-10"
         />
-        {/*<TouchableOpacity className={styles.button} onPress={getMedia}>
-          <Text className={styles.buttonText}>Get Local Media</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className={styles.button} onPress={addMediaStream}>
-          <Text className={styles.buttonText}>Add Media to Stream</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className={`${styles.button} bg-red-500`} onPress={stopLocalStream}>
-          <Text className={styles.buttonText}>Stop</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className={`${styles.button} bg-gray-600`} onPress={setup}>
-          <Text className={styles.buttonText}>Setup</Text>
-        </TouchableOpacity> */}
       </View>
     </View>
   );
